5ef6b968ba8c0e19f11efe5282528ee185d560b0
xooooxkey <134152245+xooooxkey@users.noreply.github.com>
2023-05-21 09:39:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import json
 
 def creat_release(tag_name, name, body):
     repository_owner = os.environ.get('GITHUB_REPOSITORY_OWNER')
-    repository_name = os.environ.get('GITHUB_REPOSITORY_NAME')
-    vtoken = os.environ.get('SECRETS_VOKEN')
+    repository_name = os.environ.get('GITHUB_EVENT_REPOSITORY_NAME')
+    vtoken = os.environ.get('VTOKEN')
     print(repository_owner, repository_name, vtoken)
 
     url = f'https://api.github.com/repos/{repository_owner}/{repository_name}/releases'
